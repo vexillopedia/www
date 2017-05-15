6d36d2bb4fb9b271f46fb7495f9db0367ce46f09
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2017-05-16 00:55:28 +0200

    Replace single quotes by double quotes

--- public/service-worker.js
+++ public/service-worker.js
@@ -16,7 +16,7 @@ self.addEventListener("install", function(event) {
   )
 })
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function(event) {
   event.respondWith(
     caches.match(event.request).then(function(resp) {
       return resp || fetch(event.request).then(function(response) {
