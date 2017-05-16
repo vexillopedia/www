72bd7d0ad38b91c734fee79c34c693d1f09473ef
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2017-05-16 14:03:33 +0200

    Add header check for Heroku

--- utils/redirectToHTTPS.js
+++ utils/redirectToHTTPS.js
@@ -1,7 +1,11 @@
 const redirectToHTTPS = ({ ignore = [] } = {}) => {
   return (req, res, next) => {
-    if (req.protocol === "https" || ignore.includes(req.get("host"))) {
-      next()
+    if (
+      req.protocol === "https" ||
+      req.headers['x-forwarded-proto'] === "https" ||
+      ignore.includes(req.get("host"))
+    ) {
+      return next()
     } else {
       return res.redirect("https://" + req.get("host") + req.url)
     }
